f7b2a1cbbe70962a956a1a140b93982c08bda78b
Cdelatorre <carlosdelatorrediaz@gmail.com>
2023-02-24 11:36:17 +0100

    feat: add mongodb uri variable

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const logger = require('morgan');
+require("dotenv").config();
 
-const router = require('./config/routes.config');
-require('./config/db.config');
-require('./config/hbs.config');
+const express = require("express");
+const logger = require("morgan");
+
+const router = require("./config/routes.config");
+require("./config/db.config");
+require("./config/hbs.config");
 
 const app = express();
 
-app.use(logger('dev')); // logger de morgan para ver las peticiones que se hacen
+app.use(logger("dev")); // logger de morgan para ver las peticiones que se hacen
 app.use(express.json()); // para que el body de las peticiones se pueda leer y ver en terminal
 app.use(express.urlencoded({ extended: true })); // para que el body de las peticiones se pueda leer
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'hbs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
 /** Configure static files */
 app.use(express.static("public"));
 
 /** Router **/
-app.use('/', router)
+app.use("/", router);
 
-app.listen(3000, () => console.log('App listening on port 3000!'));
+app.listen(3000, () => console.log("App listening on port 3000!"));
--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,17 @@
-const mongoose = require("mongoose")
-mongoose.set('strictQuery', true)
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-mongoose.connect("mongodb://127.0.0.1:27017/express-library", {
-  useNewUrlParser: true,
-  autoIndex: true, //make this also true
-})
-  .then(response => console.log(`Connected to Mongo! Database name: "${response.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err))
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    autoIndex: true, //make this also true
+  })
+  .then((response) =>
+    console.log(
+      `Connected to Mongo! Database name: "${response.connections[0].name}"`
+    )
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 process.on("SIGINT", function () {
   mongoose.connection.close(function () {
